1c75a01c6f631e4d53fe60515a02e85c42b1208d
Sumonta Saha Mridul <sumontasaha80@gmail.com>
2021-02-24 22:23:31 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import pyautogui
 import time
-
-while True:
-    pyautogui.typewrite('Fuck U')
+print(" Welcome to SPAM BOT !!! ")
+name = input("Enter what you wanted to spam : ")
+limit = int(input("How many time the sentence will spam : "))
+while limit > 0:
+    pyautogui.typewrite(name)
     time.sleep(5)
     pyautogui.press('enter')
+    limit = limit - 1
 
 
 
